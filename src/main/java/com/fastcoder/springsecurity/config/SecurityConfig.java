dee6295608eff79ffb18fafe2c44b88c16c6da94
Fares Snene <faressnene@gmail.com>
2023-06-28 09:48:11 +0100

    Init project with Spring Security 5.2.x that implements WebSecurityConfigurerAdapter

--- src/main/java/com/fastcoder/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/fastcoder/springsecurity/config/SecurityConfig.java
@@ -3,19 +3,24 @@ package com.fastcoder.springsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
-public class SecurityConfig {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     InMemoryUserDetailsManager inMemoryUserDetailsManager(){
@@ -31,16 +36,16 @@ public class SecurityConfig {
         );
     }
 
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorizationManager -> authorizationManager.anyRequest().authenticated())
-                //Deprecated
-                //.authorizeRequests(auth -> auth.anyRequest().authenticated())
-                //.sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .formLogin(Customizer.withDefaults())
-                //.httpBasic(Customizer.withDefaults())
-                .build();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.
+                csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+                .authorizeRequests().antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
     }
+
+
+
 }
